ae4582890a1ac0738d515f08306c8c0d56107cb7
Ivan <jazzman.ain@gmail.com>
2022-05-29 22:51:07 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() => const FlashChatApp();
+
+class FlashChatApp extends StatelessWidget {
+  const FlashChatApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
